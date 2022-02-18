22c81d8039369df755e5d6053d269e5384c414db
whitebaby <632244160@qq.com>
2022-02-18 22:29:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import requests
+# print(r.json()) 
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import time
 
 header = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36"
@@ -8,16 +15,11 @@ cookie = {
 	"PSTM": "553180542",
 	"HMACCOUNT": "BA4C08D999D27E4E"
 }
- 
+
 r = requests.get(url="https://www.ouyicn.gift/priapi/v5/rubik/web/public/spread-arbitrage?t=1645165565597&ctType=linear&arbitrageType=futures_spot", headers=header, cookies=cookie)
 rToJson = r.json()
 rData= rToJson['data']
-# print(r.json()) 
-import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
-import numpy as np
-import pandas as pd
+
 
 # import pandas as pd
 
@@ -27,5 +29,11 @@ st.write("这是我们使用数据创建表的首次尝试：")
 #     'first column': [1, 2, 3, 4],
 #     'second column': [10, 20, 30, 40]
 # })
-st.dataframe(df, width=800, height=None)
+
+st.dataframe(df, width=1200, height=500)
+
+time.sleep(10)
+st.experimental_rerun()
+
+
 
