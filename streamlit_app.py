429be2552694b91412a3c0a337c34a768099d031
whitebaby <632244160@qq.com>
2022-02-20 23:20:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,28 +16,26 @@ cookie = {
 	"HMACCOUNT": "BA4C08D999D27E4E"
 }
 
-r = requests.get(url="https://www.ouyicn.gift/priapi/v5/rubik/web/public/funding-rate-arbitrage?t=1645165302774&ctType=linear&arbitrageType=futures_spot", headers=header, cookies=cookie)
+r = requests.get(url="https://www.ouyicn.gift/priapi/v5/rubik/web/public/spread-arbitrage?t=1645165565597&ctType=linear&arbitrageType=futures_spot", headers=header, cookies=cookie)
 rToJson = r.json()
 rData= rToJson['data']
 
 
 # import pandas as pd
 
-df=pd.DataFrame(rData)
+df1=pd.DataFrame(rData)
 st.header("Arbitrage Stragety Live Data ")
 st.info("Users can click to zoom in ")
-# df = pd.DataFrame({
+# df1
+# = pd.DataFrame({
 #     'first column': [1, 2, 3, 4],
 #     'second column': [10, 20, 30, 40]
 # })
 
-st.dataframe(df, width=1200, height=500)
+st.dataframe(df1, width=1200, height=500)
 
-from pyecharts.charts import Bar
-from pyecharts.faker import Faker
-import streamlit_echarts
-from pyecharts import options as opts
-from pyecharts.globals import ThemeType
+
+from datetime import datetime
 
 def stamp2time(timestamp): #时间戳转日期函数
 
@@ -51,48 +49,27 @@ def convert_currency(value):
 	# print(new_value)
     return new_value
 
-header = {
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36"
-}
- 
-cookie = {
-	"PSTM": "553180542",
-	"HMACCOUNT": "BA4C08D999D27E4E"
-}
 
-r = requests.get(url="https://api.anchorprotocol.com/api/v1/deposit/1d", headers=header, cookies=cookie)
-rToJson = r.json()
-rData= rToJson['total_ust_deposits']
+r2 = requests.get(url="https://api.anchorprotocol.com/api/v1/deposit/1d", headers=header, cookies=cookie)
+rToJson2 = r2.json()
+rData2= rToJson2['total_ust_deposits']
 
 
-df_line=pd.DataFrame(rData)
-# xData =df_line["timestamp"].values.tolist()
+df2=pd.DataFrame(rData2)
 
-# df_line=pd.DataFrame(rData)
+st.write("")
+df2[['deposit']] = df2[['deposit']].astype('float')
+df2['deposit'] = df2['deposit'].div(10000*10000*10)
+df2["timestamp"]=df2["timestamp"].apply(stamp2time)
 
-df_line["timestamp"]=df_line["timestamp"].apply(stamp2time)
-
-df_line[['deposit']] = df_line[['deposit']].astype('float')
-df_line['deposit'] = df_line['deposit'].div(10000*10000*10)
-df_line[['deposit']] = df_line[['deposit']].astype('int')
-
-df_line = df_line.sort_index(ascending=False)
-df_line= df_line.tail(60)
-xData =df_line["timestamp"].values.tolist()
-yData =df_line["deposit"].values.tolist()
-
+df2.set_index(['timestamp'],inplace=True)
+df2 = df2.sort_index()
+data2 = df2.tail(50)
+# print(data2)
 st.header("Deposit USD Amount Live Data ")
-bar = Bar()
-bar.add_xaxis(xData)
-bar.add_yaxis("Deposit Amount USD", yData)
-bar.set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-bar.set_global_opts(title_opts=opts.TitleOpts(title="AMOUNT", subtitle="USD"))
-streamlit_echarts.st_pyecharts(
-    bar,
-    theme=ThemeType.DARK
-)
-
+st.bar_chart(data2)
 # time.sleep(10)
+# st.experimental_rerun()
 st.experimental_rerun()
 
 
